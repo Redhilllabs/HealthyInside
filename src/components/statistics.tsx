cca547dd2fb4c425a66a0c591eaa9e5fdc47a38d
ketanu0690 <ketanupadhyay40@gmail.com>
2023-09-05 21:14:57 +0530

    new

--- src/components/statistics.tsx
+++ src/components/statistics.tsx
@@ -12,6 +12,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import { Button } from "@mantine/core";
 const Statistics = () => {
   ChartJS.register(
     CategoryScale,
@@ -55,8 +56,27 @@ const Statistics = () => {
   });
 
   return (
-    <div className="container" style={{width:"50%"}}>
+    <div className="container" style={{ width: "50%" }}>
       <Bar options={options} data={userData} />
+      <div className="container">
+        <div className="d-flex mt-3">
+          <div className="m-2">
+            <Button color="blue" size="xl" uppercase>
+              MemberShip 100
+            </Button>
+          </div>
+          <div className="m-2">
+            <Button color="yellow" size="xl" uppercase>
+              Todays Sales 80
+            </Button>
+          </div>
+          <div className="m-2">
+            <Button color="red" size="xl" uppercase>
+              Todays Profits 69%
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
